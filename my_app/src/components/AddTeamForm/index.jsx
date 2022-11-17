1dabc446f1ecbe0f2ca082a3f1ce0302d776573b
KarinaK <mskarinakrivko@gmail.com>
2022-11-17 23:37:08 +0100

    team and user forms added

--- my_app/src/components/AddTeamForm/index.jsx
+++ my_app/src/components/AddTeamForm/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import s from './index.module.css'
 
 export default function AddTeamForm() {
+
+    const submit_form = event => {
+        event.preventDefault();
+        const { team_title } = event.target;
+        console.log(team_title.value);
+        team_title.value = '';
+    }
+
   return (
-    <div>AddTeamForm</div>
+    <form className={ s.add_team_form}>
+        <p>Add team</p>
+        <input type='text' name='team_title' placeholder="Team's title" />
+        <button>Add team</button>
+        </form>
   )
 }
--- my_app/src/components/AddUserForm/index.jsx
+++ my_app/src/components/AddUserForm/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import s from './index.module.css'
 
 export default function AddUserForm() {
+  
+    const submit_form2 = event => {
+        event.preventDefault();
+        const { user_title } = event.target;
+        console.log(user_title.value);
+        user_title.value = '';
+    }
+
   return (
-    <AddUserForm />
+    <form className={ s.add_user_form }>
+        <p>Add user</p>
+        <input type='text' name='user_title' placeholder="User's title" />
+        <button>Add user</button>
+        </form>
   )
 }
+
+  
